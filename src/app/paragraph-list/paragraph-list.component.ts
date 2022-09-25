e5cd1fa57fad32c1f59679900489d8d1a4b94ce3
Kalon Ohmstede <kalonohmstede@gmail.com>
2022-09-24 21:12:43 -0500

    angular basics - styling a comonent

---
+++ src/app/paragraph-list/paragraph-list.component.ts
@@ -0,0 +1,15 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'para-list',
+  templateUrl: './paragraph-list.component.html',
+  styleUrls: ['./paragraph-list.component.css']
+})
+
+export class ParagraphListComponent {
+
+
+
+
+
+}
